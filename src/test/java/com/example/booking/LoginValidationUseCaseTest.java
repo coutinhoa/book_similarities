4dd7254af9f388cfe8d73af420b524ba02e59352
A_coutinho <coutinhofalice@gmail.com>
2025-06-26 10:24:20 +0200

    fix: add parameterized tests

--- src/test/java/com/example/booking/LoginValidationUseCaseTest.java
+++ src/test/java/com/example/booking/LoginValidationUseCaseTest.java
@@ -4,9 +4,13 @@ import com.example.booking.domain.LoginValidationUseCase;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -30,5 +34,21 @@ class LoginValidationUseCaseTest {
     void validateLogin_shouldReturnFalse_forInvalidEmails(String invalidEmail) {
         boolean result = loginValidationUseCase.validateLogin(invalidEmail);
         assertFalse(result);
+    }
+
+
+    @ParameterizedTest(name = "{index} => input=''{0}'', expected=''{1}''")
+    @MethodSource("arguments")
+    void testingParameterizedTests(String input, boolean expected) {
+        boolean result = loginValidationUseCase.validateLogin(input);
+        assertEquals(expected, result);
+    }
+
+    private static Stream<Arguments> arguments() {
+        return Stream.of(
+                Arguments.of("invalid-email-format", false),
+                Arguments.of("email@invalid.com", false),
+                Arguments.of("email@email.com", true)
+        );
     }
 }
